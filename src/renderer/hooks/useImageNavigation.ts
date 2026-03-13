ec49d0425dbc9911cfe49d93f3f90df0d7df66b9
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-13 22:03:25 +0000

    Add new image navigation props

--- src/renderer/hooks/useImageNavigation.ts
+++ src/renderer/hooks/useImageNavigation.ts
@@ -1,15 +1,23 @@
 import { useWorkspaceStore } from '../stores/workspaceStore'
 
 export function useImageNavigation() {
-  const { currentIndex, images, nextImage, prevImage, setCurrentIndex } = useWorkspaceStore()
+  const {
+    currentIndex, images, nextImage, prevImage, setCurrentIndex,
+    deletedIndex, deletedImages, nextDeletedImage, prevDeletedImage, setDeletedIndex,
+    viewMode
+  } = useWorkspaceStore()
+
+  const isDeleted = viewMode === 'deleted'
+  const activeIndex = isDeleted ? deletedIndex : currentIndex
+  const activeImages = isDeleted ? deletedImages : images
 
   return {
-    currentIndex,
-    total: images.length,
-    hasNext: currentIndex < images.length - 1,
-    hasPrev: currentIndex > 0,
-    nextImage,
-    prevImage,
-    goTo: setCurrentIndex
+    currentIndex: activeIndex,
+    total: activeImages.length,
+    hasNext: activeIndex < activeImages.length - 1,
+    hasPrev: activeIndex > 0,
+    nextImage: isDeleted ? nextDeletedImage : nextImage,
+    prevImage: isDeleted ? prevDeletedImage : prevImage,
+    goTo: isDeleted ? setDeletedIndex : setCurrentIndex
   }
 }
