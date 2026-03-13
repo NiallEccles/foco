57ecdf05d490dec875ffb4a0849cb39c6582e205
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-13 21:36:01 +0000

    Refactor useHotkeys usage and add delete

--- src/renderer/hooks/useKeyboardShortcuts.ts
+++ src/renderer/hooks/useKeyboardShortcuts.ts
@@ -1,14 +1,41 @@
 import { useHotkeys } from 'react-hotkeys-hook'
 import { useWorkspaceStore } from '../stores/workspaceStore'
 import { useEditorStore } from '../stores/editorStore'
+import { useViewerStore } from '../stores/viewerStore'
 
 export function useKeyboardShortcuts() {
-  const { nextImage, prevImage, softDeleteCurrent } = useWorkspaceStore()
+  const { nextImage, prevImage, softDeleteCurrent, nextDeletedImage, prevDeletedImage, restoreCurrentDeleted, exitDeletedView, viewMode } = useWorkspaceStore()
   const { isEditing, enterEditMode, exitEditMode } = useEditorStore()
+  const { requestReset } = useViewerStore()
 
-  useHotkeys('right, j', () => { if (!isEditing) nextImage() }, { preventDefault: true })
-  useHotkeys('left, k', () => { if (!isEditing) prevImage() }, { preventDefault: true })
-  useHotkeys('d, delete', () => { if (!isEditing) softDeleteCurrent() }, { preventDefault: true })
-  useHotkeys('e', () => { if (!isEditing) enterEditMode('crop') }, { preventDefault: true })
-  useHotkeys('escape', () => { if (isEditing) exitEditMode() }, { preventDefault: true })
+  const isDeleted = viewMode === 'deleted'
+
+  useHotkeys('right, j', () => {
+    if (isEditing) return
+    if (isDeleted) nextDeletedImage()
+    else nextImage()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('left, k', () => {
+    if (isEditing) return
+    if (isDeleted) prevDeletedImage()
+    else prevImage()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('d, delete', () => {
+    if (!isEditing && !isDeleted) softDeleteCurrent()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('r', () => {
+    if (!isEditing && isDeleted) restoreCurrentDeleted()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('e', () => { if (!isEditing && !isDeleted) enterEditMode('crop') }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('escape', () => {
+    if (isEditing) exitEditMode()
+    else if (isDeleted) exitDeletedView()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('0', () => { if (!isEditing) requestReset() }, { preventDefault: true })
 }
