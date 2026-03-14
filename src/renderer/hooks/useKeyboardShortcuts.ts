480f6c0ad56b3d45c236530865c9ba020c166677
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-14 15:08:49 +0000

    Change nav keybinds based on orientation. Add exif panel open

--- src/renderer/hooks/useKeyboardShortcuts.ts
+++ src/renderer/hooks/useKeyboardShortcuts.ts
@@ -2,21 +2,24 @@ import { useHotkeys } from 'react-hotkeys-hook'
 import { useWorkspaceStore } from '../stores/workspaceStore'
 import { useEditorStore } from '../stores/editorStore'
 import { useViewerStore } from '../stores/viewerStore'
+import { useSettingsStore } from '../stores/settingsStore'
 
 export function useKeyboardShortcuts() {
   const { nextImage, prevImage, softDeleteCurrent, nextDeletedImage, prevDeletedImage, restoreCurrentDeleted, exitDeletedView, viewMode } = useWorkspaceStore()
   const { isEditing, enterEditMode, exitEditMode } = useEditorStore()
   const { requestReset } = useViewerStore()
+  const { filmStripOrientation, toggleExifPanel } = useSettingsStore()
 
   const isDeleted = viewMode === 'deleted'
+  const isVertical = ['left', 'right'].includes(filmStripOrientation)
 
-  useHotkeys('right, j', () => {
+  useHotkeys(isVertical ? 'down, j' : 'right, j', () => {
     if (isEditing) return
     if (isDeleted) nextDeletedImage()
     else nextImage()
   }, { preventDefault: true }, [isEditing, isDeleted])
 
-  useHotkeys('left, k', () => {
+  useHotkeys(isVertical ? 'up, k' : 'left, k', () => {
     if (isEditing) return
     if (isDeleted) prevDeletedImage()
     else prevImage()
@@ -38,4 +41,6 @@ export function useKeyboardShortcuts() {
   }, { preventDefault: true }, [isEditing, isDeleted])
 
   useHotkeys('0', () => { if (!isEditing) requestReset() }, { preventDefault: true })
+
+  useHotkeys('i', () => { if (!isEditing) toggleExifPanel() }, { preventDefault: true }, [isEditing])
 }
