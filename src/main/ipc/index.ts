4058fe8fe09f9fcd2066da1cfe81371cd9fb9ec6
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-14 15:36:09 +0000

    Add registerWindowHandlers

--- src/main/ipc/index.ts
+++ src/main/ipc/index.ts
@@ -1,7 +1,9 @@
 import { registerFileHandlers } from './fileHandlers'
 import { registerImageHandlers } from './imageHandlers'
+import { registerWindowHandlers } from './windowHandlers'
 
 export function registerIpcHandlers(): void {
   registerFileHandlers()
   registerImageHandlers()
+  registerWindowHandlers()
 }
