d582cf6752f83745d8cf41493f39bb34fe4424b5
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-14 15:37:33 +0000

    Add title bar and exif panel

--- src/renderer/components/layout/AppShell.tsx
+++ src/renderer/components/layout/AppShell.tsx
@@ -1,11 +1,13 @@
 import { AppShell as MantineAppShell } from '@mantine/core'
 import { Sidebar } from './Sidebar'
 import { StatusBar } from './StatusBar'
+import { TitleBar } from './TitleBar'
 import { ImageViewer } from '../viewer/ImageViewer'
 import { ViewerToolbar } from '../viewer/ViewerToolbar'
 import { FilmStrip } from '../gallery/FilmStrip'
 import { EmptyState } from '../gallery/EmptyState'
 import { EditorCanvas } from '../editor/EditorCanvas'
+import { ExifPanel } from '../viewer/ExifPanel'
 import { useImages } from '../../hooks/useImages'
 import { useKeyboardShortcuts } from '../../hooks/useKeyboardShortcuts'
 import { useEditorStore } from '../../stores/editorStore'
@@ -14,23 +16,37 @@ import { useSettingsStore } from '../../stores/settingsStore'
 export function AppLayout() {
   const { currentImage, folderPath, viewMode } = useImages()
   const { isEditing } = useEditorStore()
-  const { filmStripOrientation } = useSettingsStore()
+  const { filmStripOrientation, exifPanelOpen } = useSettingsStore()
   useKeyboardShortcuts()
+
+  const showExifPanel = exifPanelOpen && !isEditing && !!currentImage
 
   return (
     <MantineAppShell
+      header={{ height: 38 }}
       navbar={{ width: 220, breakpoint: 'sm' }}
+      aside={showExifPanel ? { width: 260, breakpoint: 'sm' } : undefined}
       style={{ height: '100vh' }}
     >
+      <MantineAppShell.Header style={{ padding: 0, border: 'none' }}>
+        <TitleBar />
+      </MantineAppShell.Header>
+
       <MantineAppShell.Navbar>
         <Sidebar />
       </MantineAppShell.Navbar>
+
+      {showExifPanel && (
+        <MantineAppShell.Aside>
+          <ExifPanel />
+        </MantineAppShell.Aside>
+      )}
 
       <MantineAppShell.Main
         style={{
           display: 'flex',
           flexDirection: 'column',
-          height: '100vh',
+          height: 'calc(100vh - var(--app-shell-header-height, 38px))',
           paddingTop: 0
         }}
       >
