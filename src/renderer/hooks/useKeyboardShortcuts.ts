7ae8b1eb6c50d1b19ed3a42fa6d8368b410e5091
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-20 23:04:32 +0000

    Add more shortcuts

--- src/renderer/hooks/useKeyboardShortcuts.ts
+++ src/renderer/hooks/useKeyboardShortcuts.ts
@@ -5,10 +5,10 @@ import { useViewerStore } from '../stores/viewerStore'
 import { useSettingsStore } from '../stores/settingsStore'
 
 export function useKeyboardShortcuts() {
-  const { nextImage, prevImage, softDeleteCurrent, nextDeletedImage, prevDeletedImage, restoreCurrentDeleted, exitDeletedView, viewMode } = useWorkspaceStore()
+  const { nextImage, prevImage, softDeleteCurrent, nextDeletedImage, prevDeletedImage, restoreCurrentDeleted, exitDeletedView, enterDeletedView, viewMode } = useWorkspaceStore()
   const { isEditing, enterEditMode, exitEditMode } = useEditorStore()
-  const { requestReset } = useViewerStore()
-  const { filmStripOrientation, toggleExifPanel } = useSettingsStore()
+  const { requestReset, requestZoomIn, requestZoomOut } = useViewerStore()
+  const { filmStripOrientation, toggleExifPanel, toggleHistogram, toggleShortcuts } = useSettingsStore()
 
   const isDeleted = viewMode === 'deleted'
   const isVertical = ['left', 'right'].includes(filmStripOrientation)
@@ -25,12 +25,20 @@ export function useKeyboardShortcuts() {
     else prevImage()
   }, { preventDefault: true }, [isEditing, isDeleted])
 
+  useHotkeys('space', () => {
+    if (!isEditing && !isDeleted) nextImage()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
   useHotkeys('d, delete', () => {
     if (!isEditing && !isDeleted) softDeleteCurrent()
   }, { preventDefault: true }, [isEditing, isDeleted])
 
   useHotkeys('r', () => {
     if (!isEditing && isDeleted) restoreCurrentDeleted()
+  }, { preventDefault: true }, [isEditing, isDeleted])
+
+  useHotkeys('v', () => {
+    if (!isEditing && !isDeleted) enterDeletedView()
   }, { preventDefault: true }, [isEditing, isDeleted])
 
   useHotkeys('e', () => { if (!isEditing && !isDeleted) enterEditMode('crop') }, { preventDefault: true }, [isEditing, isDeleted])
@@ -42,5 +50,11 @@ export function useKeyboardShortcuts() {
 
   useHotkeys('0', () => { if (!isEditing) requestReset() }, { preventDefault: true })
 
+  useHotkeys('equal, shift+equal', () => { if (!isEditing) requestZoomIn() }, { preventDefault: true }, [isEditing])
+  useHotkeys('minus', () => { if (!isEditing) requestZoomOut() }, { preventDefault: true }, [isEditing])
+
   useHotkeys('i', () => { if (!isEditing) toggleExifPanel() }, { preventDefault: true }, [isEditing])
+  useHotkeys('h', () => { if (!isEditing) toggleHistogram() }, { preventDefault: true }, [isEditing])
+  useHotkeys('f', () => { window.api.toggleFullscreen() }, { preventDefault: true })
+  useHotkeys('shift+slash', () => { toggleShortcuts() }, { preventDefault: true })
 }
