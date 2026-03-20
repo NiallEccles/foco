ed75b14520d3a84e635518e32e81627b898ac6a6
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-20 23:05:07 +0000

    Add zoom properties to store

--- src/renderer/stores/viewerStore.ts
+++ src/renderer/stores/viewerStore.ts
@@ -5,11 +5,19 @@ interface ViewerState {
   setZoomPercent: (z: number) => void
   resetRequestCount: number
   requestReset: () => void
+  zoomInCount: number
+  requestZoomIn: () => void
+  zoomOutCount: number
+  requestZoomOut: () => void
 }
 
 export const useViewerStore = create<ViewerState>()((set) => ({
   zoomPercent: 100,
   setZoomPercent: (z) => set({ zoomPercent: z }),
   resetRequestCount: 0,
-  requestReset: () => set((s) => ({ resetRequestCount: s.resetRequestCount + 1 }))
+  requestReset: () => set((s) => ({ resetRequestCount: s.resetRequestCount + 1 })),
+  zoomInCount: 0,
+  requestZoomIn: () => set((s) => ({ zoomInCount: s.zoomInCount + 1 })),
+  zoomOutCount: 0,
+  requestZoomOut: () => set((s) => ({ zoomOutCount: s.zoomOutCount + 1 })),
 }))
